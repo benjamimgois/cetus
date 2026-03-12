9097be9e189dad2d518ad02836404bac34d91f56
Benjamim Gois <benjamim.gois@gmail.com>
2026-03-12 10:09:00 -0300

    feat: Release version 1.6
    - Bump VERSION to 1.6
    - New official app icon: omnicom_icon.svg (gradient O with C cutout)
    - Generate PNG icons (32/48/64/128/256/512px) from new SVG
    - Serial tab: port type toggle buttons (USB / Serial)
    - Standardize button widths: 90px / 110px across all tabs
    - Traceroute: rename "ICMP (MTR)" to "ICMP"
    - Rename "Save Current" to "Save", "Delete Selected" to "Delete"
    - TFTP tab: remove Copy Name button
    - Speed tab: larger fonts for ISP/Country/Server; remove ISP Rating
    - Speed tab: Country populated via ip-api.com geolocation
    - Translate all remaining UI strings and comments to English
    - Update debian/changelog, metainfo.xml and README for 1.6

--- scripts/make-deb.sh
+++ scripts/make-deb.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-VERSION="1.5"
+VERSION="1.6"
 PKGNAME="omnicom"
 
 echo "=== Omnicom Debian Package Builder ==="
--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-VERSION="1.4"
+VERSION="1.6"
 PKGNAME="omnicom"
 BUILD_DIR="/tmp/${PKGNAME}-build"
 
